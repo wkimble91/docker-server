128fdabab0c2e4ca2dfc7778af0efb73f97f1109
Will <mail@willkimble.com>
2024-08-10 13:44:01 -0700

    Updated docker-backup script

--- .docker-backup.sh
+++ .docker-backup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Define the paths
-SERVER_OLD="/mnt/synology-nas/Files/personal/backups/server-old"
-SERVER="/mnt/synology-nas/Files/personal/backups/server"
+SERVER_2="/mnt/synology-nas/Files/personal/backups/server-home-2"
+SERVER_1="/mnt/synology-nas/Files/personal/backups/server-home-1"
 HOME_DIR="/home/will"
 
 perform_backup() {
     # Step 1: Delete everything (including dot files) in the server-old folder
-    echo -e "\033[0;31mDeleting contents in SERVER_OLD...\033[0m"
-    sudo find "$SERVER_OLD" -mindepth 1 -delete
+    echo -e "\033[0;31mDeleting contents in SERVER_2...\033[0m"
+    sudo find "$SERVER_2" -mindepth 1 -delete
     echo "Done"
     sleep 2
 
     # Step 2: Move everything (including dot files) from the server folder to the server-old folder
-    echo -e "\033[0;33mMoving all contents from SERVER to SERVER_OLD...\033[0m"
-    sudo find "$SERVER" -mindepth 1 -maxdepth 1 -exec mv -t "$SERVER_OLD" {} +
+    echo -e "\033[0;33mMoving all contents from SERVER_1 to SERVER_2...\033[0m"
+    sudo find "$SERVER_1" -mindepth 1 -maxdepth 1 -exec mv -t "$SERVER_2" {} +
     echo "Done"
     sleep 2
 
@@ -25,15 +25,15 @@ perform_backup() {
     sleep 2
 
     # Step 4: Copy everything (including dot files) from /home/will to the server folder
-    echo -e "\033[0;32mCopying all contents from HOME to SERVER...\033[0m"
-    sudo cp -r "$HOME_DIR/." "$SERVER/"
+    echo -e "\033[0;32mCopying all contents from HOME to SERVER_1...\033[0m"
+    sudo cp -r "$HOME_DIR/." "$SERVER_1/"
     echo "Done"
     sleep 2
 
     # Step 5: Create a file with a timestamp as its name in the server folder
     TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
     echo "Creating a file with the timestamp $TIMESTAMP in SERVER..."
-    sudo touch "$SERVER/backedup_on_$TIMESTAMP"
+    sudo touch "$SERVER_1/backedup_on_$TIMESTAMP"
     sleep 2
 
     # Step 6: Restarting all Docker containers
